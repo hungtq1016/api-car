fcda374475468f79d58db149fc314852424edd20
hungtq1016 <hungtq1016@gmail.com>
2023-09-15 14:57:57 +0700

    update

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Feature extends Model
@@ -18,5 +19,10 @@ class Feature extends Model
     public function cars():BelongsToMany
     {
         return $this->belongsToMany(Owner::class,'feature_car','feature_id');
+    }
+
+    public function image() : BelongsTo 
+    {
+        return $this->belongsTo(Image::class);    
     }
 }
